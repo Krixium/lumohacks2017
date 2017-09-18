5d8715614079d59a965ec25ecb4fa00e674ba9c2
Benny Wang <bennywang75@hotmail.com>
2017-09-18 15:34:39 -0700

    Removed depreciated class

--- Invoker/app/src/main/java/ca/bcit/cst/seta2016/invoker/CalendarActivity.java
+++ Invoker/app/src/main/java/ca/bcit/cst/seta2016/invoker/CalendarActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.widget.CalendarView;
 
 
@@ -13,6 +12,7 @@ public class CalendarActivity extends AppCompatActivity {
 
     private  static final String TAG = "CalendarActivity";
     private CalendarView mCalendarView;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
